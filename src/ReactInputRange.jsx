d22e56669b81c1ce593c9e3bc7439969405f7e9c
hayitmurod707 <hayitmurod707@gmail.com>
2023-04-11 11:04:01 +0500

    optimizated code

--- src/ReactInputRange.jsx
+++ src/ReactInputRange.jsx
@@ -2,37 +2,39 @@ import { bool, func, number, shape } from 'prop-types';
 import Slider from 'react-input-range';
 import styled from 'styled-components';
 const StyledElement = styled.div`
-	& .input-range__slider-container,
-	& .input-range__track {
-		transition: none;
-	}
+   & .input-range__slider-container,
+   & .input-range__track {
+      transition: none;
+   }
 `;
 const defaultOptions = {
-	// formatLabel
-	// name
-	// onChangeStart
-	// onChangeComplete
+   // formatLabel
+   // name
+   // onChangeStart
+   // onChangeComplete
 };
+
 // renderTrack
 // renderThumb
+
 const ReactInputRange = props => (
-	<StyledElement>
-		<Slider {...defaultOptions} {...props} />
-	</StyledElement>
+   <StyledElement>
+      <Slider {...defaultOptions} {...props} />
+   </StyledElement>
 );
 ReactInputRange.defaultProps = {
-	disabled: false,
-	minValue: 0,
-	step: 1,
-	value: { min: 0, max: 100 },
-	maxValue: 100,
+   disabled: false,
+   minValue: 0,
+   step: 1,
+   value: { min: 0, max: 100 },
+   maxValue: 100,
 };
 ReactInputRange.propTypes = {
-	disabled: bool,
-	onChange: func.isRequired,
-	step: number,
-	value: shape({ min: number.isRequired, max: number.isRequired }).isRequired,
-	maxValue: number,
-	minValue: number,
+   disabled: bool,
+   onChange: func.isRequired,
+   step: number,
+   value: shape({ min: number.isRequired, max: number.isRequired }).isRequired,
+   maxValue: number,
+   minValue: number,
 };
 export default ReactInputRange;
